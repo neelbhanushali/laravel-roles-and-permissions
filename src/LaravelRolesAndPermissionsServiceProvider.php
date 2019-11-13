2e88e1d22cd3770c04fe9d5c0dc9315b0ae5efef
Neel Bhanushali <neal.bhanushali@gmail.com>
2019-11-13 23:13:06 +0530

    service provider

---
+++ src/LaravelRolesAndPermissionsServiceProvider.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace NeelBhanushali\LaravelRolesAndPermissions;
+
+use Illuminate\Support\ServiceProvider;
+
+class LaravelRolesAndPermissionsServiceProvider extends ServiceProvider
+{
+    /**
+     * Register services.
+     *
+     * @return void
+     */
+    public function register()
+    {
+        //
+    }
+
+    /**
+     * Bootstrap services.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+        //
+    }
+}
