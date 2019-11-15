dc282f882200c1b035c42e35ef9dcec366c747d6
Neel Bhanushali <neal.bhanushali@gmail.com>
2019-11-15 13:51:25 +0530

    hasRolesAndPermissions relation fixed

--- src/Traits/HasRolesAndPermissions.php
+++ src/Traits/HasRolesAndPermissions.php
@@ -2,6 +2,7 @@
 
 namespace NeelBhanushali\LaravelRolesAndPermissions\Traits;
 
+use NeelBhanushali\LaravelRolesAndPermissions\Models\ModelPermission;
 use NeelBhanushali\LaravelRolesAndPermissions\Models\ModelRole;
 use NeelBhanushali\LaravelRolesAndPermissions\Models\Permission;
 use NeelBhanushali\LaravelRolesAndPermissions\Models\Role;
@@ -10,11 +11,11 @@ trait HasRolesAndPermissions
 {
     public function roles()
     {
-        return $this->morphToMany(Role::class, 'entity', 'model_roles');
+        return $this->morphToMany(Role::class, 'entity', 'model_roles', null, 'role_id')->withTimestamps();
     }
 
     public function permissions()
     {
-        return $this->morphToMany(Permission::class, 'entity', 'model_permissions');
+        return $this->morphToMany(Permission::class, 'entity', 'model_permissions', null, 'permission_id')->withPivot('is_revoked');
     }
 }
