6a3925b8614cf32e19ed74af122308f0216bb854
Neel Bhanushali <neal.bhanushali@gmail.com>
2019-11-18 00:20:23 +0530

    get roles and get permission function in trait

--- src/Traits/HasRolesAndPermissions.php
+++ src/Traits/HasRolesAndPermissions.php
@@ -9,18 +9,51 @@ use NeelBhanushali\LaravelRolesAndPermissions\Models\Role;
 
 trait HasRolesAndPermissions
 {
-    public function model_roles()
+    public function roles()
     {
         return $this->morphToMany(Role::class, 'entity', 'model_roles', null, 'role_id')
             ->withTimestamps()
             ->using(ModelRole::class);
     }
 
-    public function model_permissions()
+    public function permissions()
     {
         return $this->morphToMany(Permission::class, 'entity', 'model_permissions', null, 'permission_id')
             ->withTimestamps()
             ->withPivot('is_revoked')
             ->using(ModelPermission::class);
+    }
+
+    public function getRoles()
+    {
+        return $this->roles->pluck('name');
+    }
+
+    public function getPermissions()
+    {
+        $permissions_from_roles = [];
+        foreach ($this->roles as $role) {
+            foreach ($role->getPermissions() as $permission) {
+                $permissions_from_roles[] = $permission;
+            }
+        }
+        $permissions_from_roles = array_values(array_unique($permissions_from_roles));
+
+        $model_permissions = [];
+        foreach ($this->permissions as $permission) {
+            if (!$permission->pivot->is_revoked) {
+                $model_permissions[] = $permission->name;
+            }
+        }
+
+        $all_permissions = array_merge($permissions_from_roles, $model_permissions);
+
+        foreach ($this->permissions()->where('is_revoked', 1)->get() as $revoked_permission) {
+            $all_permissions = array_filter($all_permissions, function ($permission) use ($revoked_permission) {
+                return $permission != $revoked_permission->name;
+            });
+        }
+
+        return array_values($all_permissions);
     }
 }
