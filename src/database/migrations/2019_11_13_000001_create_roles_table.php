2bde03b219bc71800b4741cc319c962ea4dfbeb7
Neel Bhanushali <neal.bhanushali@gmail.com>
2019-11-25 13:43:51 +0530

    is_visible_to_users boolean

--- src/database/migrations/2019_11_13_000001_create_roles_table.php
+++ src/database/migrations/2019_11_13_000001_create_roles_table.php
@@ -18,7 +18,7 @@ class CreateRolesTable extends Migration
             $table->string('entity_type')->nullable();
             $table->string('entity_id')->nullable();
             $table->string('name');
-            $table->boolean('is_global')->default(0);
+            $table->boolean('is_visible_to_users')->default(1);
             $table->timestamps();
         });
     }
