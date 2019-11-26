d18b0fb56c2e3ec44dc0b8fa3b7597ab1bc57735
Neel Bhanushali <neal.bhanushali@gmail.com>
2019-11-26 23:27:23 +0530

    added scopes

--- src/Models/Permission.php
+++ src/Models/Permission.php
@@ -3,8 +3,26 @@
 namespace NeelBhanushali\LaravelRolesAndPermissions\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Builder;
 
 class Permission extends Model
 {
-    // 
+    public function scopeGlobal($query)
+    {
+        return $query->whereNull('entity_id')->whereNull('entity_type');
+    }
+
+    protected static function boot()
+    {
+        parent::boot();
+
+        static::addGlobalScope('visible', function (Builder $builder) {
+            $builder->where('is_visible_to_users', 1);
+        });
+    }
+
+    public function scopeInvisible($query)
+    {
+        return $query->where('is_visible_to_users', 0);
+    }
 }
--- src/Models/Role.php
+++ src/Models/Role.php
@@ -3,6 +3,7 @@
 namespace NeelBhanushali\LaravelRolesAndPermissions\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Builder;
 
 class Role extends Model
 {
@@ -14,5 +15,24 @@ class Role extends Model
     public function getPermissions()
     {
         return $this->permissions->pluck('name');
+    }
+
+    public function scopeGlobal($query)
+    {
+        return $query->whereNull('entity_id')->whereNull('entity_type');
+    }
+
+    protected static function boot()
+    {
+        parent::boot();
+
+        static::addGlobalScope('visible', function (Builder $builder) {
+            $builder->where('is_visible_to_users', 1);
+        });
+    }
+
+    public function scopeInvisible($query)
+    {
+        return $query->where('is_visible_to_users', 0);
     }
 }
